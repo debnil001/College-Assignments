5dadc98225dcf7004d64ae5768ff2452a8aa1ef0
debnil001 <93644973+debnil001@users.noreply.github.com>
2023-06-10 09:50:10 +0530

    Update prog5.sh

--- OS Assignment 3/prog5.sh
+++ OS Assignment 3/prog5.sh
@@ -1,7 +1,19 @@
-files=`ls *.sh`
-exten="exe"
-count=0
-for file in $files
-do
-	mv $file ${file%.sh}.$exten
-done 
+ls -l $1
+src=$1
+flag=0
+ex=exe
+for file in *.sh;do
+	if cmp -s "$file" "$src"
+	then
+		flag=1
+		mv "$src" "${file%.sh}.exe"
+	fi
+done
+
+if [ $flag -eq 0 ]
+then
+	echo "No such File '$src' existed"
+else
+	echo "$src is renamed to ${src%.sh}.exe"
+fi
+
