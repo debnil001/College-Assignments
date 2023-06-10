4ad2bde522383373d0448b3ef6a1200ede758d4e
debnil001 <93644973+debnil001@users.noreply.github.com>
2023-06-10 09:49:24 +0530

    Update prog4.sh

--- OS Assignment 3/prog4.sh
+++ OS Assignment 3/prog4.sh
@@ -1,1 +1,17 @@
-find . -name '*~' -type f -delete
+flag=0
+ls
+for file in *~;do
+	if [ -f $file ]
+	then
+		echo "$file is deleted"
+		rm $file
+		flag=1
+	fi
+done
+if [ $flag -eq 0 ]
+then
+	echo "No temporary files in directory"
+else
+	echo "All temporary files are deleted"
+fi 
+
