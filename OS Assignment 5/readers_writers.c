f1171fe7f11bce2e96b850d4803a24e235720841
debnil001 <93644973+debnil001@users.noreply.github.com>
2023-06-12 08:36:48 +0530

    Update readers_writers.c

--- OS Assignment 5/readers_writers.c
+++ OS Assignment 5/readers_writers.c
@@ -51,3 +51,77 @@ int main(){
     sem_destroy(&wrt);
     return 0;
 }
+
+
+
+#include <stdio.h>
+#include <pthread.h>
+
+#define MAX_READERS 3
+
+int readers_count = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t read_condition = PTHREAD_COND_INITIALIZER;
+
+void *reader(void *arg) {
+    // Entry section
+    pthread_mutex_lock(&mutex);
+
+    // Wait if maximum number of readers are already in the CS
+    while (readers_count >= MAX_READERS) {
+        pthread_cond_wait(&read_condition, &mutex);
+    }
+
+    // Increment readers count
+    readers_count++;
+
+    // Release the lock
+    pthread_mutex_unlock(&mutex);
+
+    // Critical section (reading)
+    printf("Reader %ld in critical section\n", (long)arg);
+
+    // Exit section
+    pthread_mutex_lock(&mutex);
+
+    // Decrement readers count
+    readers_count--;
+
+    // Signal the waiting readers that a slot is available
+    pthread_cond_broadcast(&read_condition);
+
+    // Release the lock
+    pthread_mutex_unlock(&mutex);
+
+    // Rest of the code (after critical section)
+    return NULL;
+}
+
+void *writer(void *arg) {
+    // Writer code
+    // ...
+    return NULL;
+}
+
+int main() {
+    pthread_t reader_threads[5];
+    pthread_t writer_thread;
+
+    // Create reader threads
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&reader_threads[i], NULL, reader, (void *)(long)i);
+    }
+
+    // Create writer thread
+    pthread_create(&writer_thread, NULL, writer, NULL);
+
+    // Join reader threads
+    for (int i = 0; i < 5; i++) {
+        pthread_join(reader_threads[i], NULL);
+    }
+
+    // Join writer thread
+    pthread_join(writer_thread, NULL);
+
+    return 0;
+}
