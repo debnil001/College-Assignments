29eb9dbc3cca7ba1ef1901d4e1ef9f0fa31ae434
debnil001 <93644973+debnil001@users.noreply.github.com>
2023-06-12 11:02:36 +0530

    Update readers_writers.c

--- OS Assignment 5/readers_writers.c
+++ OS Assignment 5/readers_writers.c
@@ -125,3 +125,94 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_READERS 3
+
+int shared_data = 0;
+int readers_count = 0;
+sem_t mutex;
+sem_t readers_sem;
+
+void *reader(void *arg)
+{
+    int reader_id = *(int *)arg;
+    while (1) {
+        sem_wait(&readers_sem);  // Wait until there's room for the reader
+        sem_wait(&mutex);        // Acquire the mutex to update readers_count
+        readers_count++;
+        if (readers_count == 1) {
+            // If this is the first reader, wait for any writers to finish
+            sem_wait(&readers_sem);
+        }
+        sem_post(&mutex);        // Release the mutex
+
+        // Reading the shared data
+        printf("Reader %d reads: %d\n", reader_id, shared_data);
+
+        sem_wait(&mutex);        // Acquire the mutex to update readers_count
+        readers_count--;
+        if (readers_count == 0) {
+            // If this is the last reader, allow writers to proceed
+            sem_post(&readers_sem);
+        }
+        sem_post(&mutex);        // Release the mutex
+    }
+}
+
+void *writer(void *arg)
+{
+    int writer_id = *(int *)arg;
+    while (1) {
+        sem_wait(&readers_sem);  // Wait until there's room for the writer
+
+        // Writing to the shared data
+        shared_data++;
+        printf("Writer %d writes: %d\n", writer_id, shared_data);
+
+        sem_post(&readers_sem);  // Release the readers semaphore
+    }
+}
+
+int main()
+{
+    pthread_t readers[MAX_READERS];
+    pthread_t writers;
+    int reader_ids[MAX_READERS];
+    int writer_id = 1;
+
+    sem_init(&mutex, 0, 1);
+    sem_init(&readers_sem, 0, MAX_READERS);
+
+    // Create reader threads
+    for (int i = 0; i < MAX_READERS; i++) {
+        reader_ids[i] = i + 1;
+        pthread_create(&readers[i], NULL, reader, &reader_ids[i]);
+    }
+
+    // Create writer thread
+    pthread_create(&writers, NULL, writer, &writer_id);
+
+    // Wait for threads to finish (this is an infinite loop)
+    for (int i = 0; i < MAX_READERS; i++) {
+        pthread_join(readers[i], NULL);
+    }
+    pthread_join(writers, NULL);
+
+    // Destroy semaphores
+    sem_destroy(&mutex);
+    sem_destroy(&readers_sem);
+
+    return 0;
+}
+
